3ccd407b94f24e2267666bfeb0551d74401429fd
Ryan <ryan.3feitosa@gmail.com>
2024-12-09 08:19:39 -0300

    Adicionei as classes Services

---
+++ src/main/java/com/sistemamultiversa/ProjetoMultiversa/repositorio/ImovelRepository.java
@@ -0,0 +1,9 @@
+package com.sistemamultiversa.ProjetoMultiversa.repositorio;
+
+import com.sistemamultiversa.ProjetoMultiversa.model.ImovelModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImovelRepository extends JpaRepository<ImovelModel, Long> {
+}
